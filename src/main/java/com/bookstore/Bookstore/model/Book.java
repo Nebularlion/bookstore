964c22cd4040bffaf6a1cc94f42c04437df242be
Nebularlion <elina.rajala@hotmail.fi>
2018-01-30 23:58:27 +0200

    Project init

---
+++ src/main/java/com/bookstore/Bookstore/model/Book.java
@@ -0,0 +1,50 @@
+package com.bookstore.Bookstore.model;
+
+public class Book {
+
+    private String title;
+    private String author;
+    private String year;
+    private int isbn;
+    private double price;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
